1e7a57329e6d8b2295fc297006a958c49d5cc238
max <yr.jiao@outlook.com>
2023-09-19 21:27:10 +0800

    Start page beautify

--- src/main.js
+++ src/main.js
@@ -3,8 +3,19 @@ import App from './App.vue'
 import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css';
+import Cookies from 'js-cookie';
 Vue.config.productionTip = false
 Vue.use(ElementUI)
+
+
+// global befored导航守卫
+router.beforeEach((to, from, next) => {
+  const token = Cookies.get('token')
+  if (token && to.path === '/login') {
+    next({ path: '/' })
+  } else
+    next()
+})
 new Vue({
   router,
   render: h => h(App),
